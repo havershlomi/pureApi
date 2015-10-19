6fa991fa816ac487d1dca0343347b29d5ab7a76f
Shlomi Haver <shlomih@appollocretive.com>
2015-10-20 00:29:29 +0300

    added functions to apiresponsse

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ var requireDir = require('require-dir'),
     extend = require("extend");
 
 var CustomError = require("./lib/customError"),
-    ErrorLogger = require("./lib/errorLogger"),
     ApiResponse = require("./lib/apiResponse");
 
 exports.CustomError = CustomError;
-exports.ErrorLogger = ErrorLogger;
 exports.ApiResponse = ApiResponse;
 
 var defaults = {
@@ -25,19 +23,19 @@ exports.endPoint = function (options) {
     var handlers = requireDir(options.handlersDir);
 
     return function (req, res, next) {
-        var errorLog = new ErrorLogger(),
+        var apiResponse = new ApiResponse(),
             dataType = options.dataType,
             actionStr = options.actionStr;
 
         var data = req.body || {};
         if (!options.validateReqFunc(req)) {
-            errorLog.insert(new CustomError(400, "Invalid_data", CustomError.NOT_IN_THE_RIGTH_FORMAT, "The request is not in the right format"));
+            apiResponse.addLog(new CustomError(400, "Invalid_data", CustomError.NOT_IN_THE_RIGTH_FORMAT, "The request is not in the right format"));
         } else if (handlers[data[dataType]] === undefined) {
-            errorLog.insert(new CustomError(400, "Invalid_data", CustomError.MODEL_TYPE_NOT_EXIST, "Model is not exist"));
+            apiResponse.addLog(new CustomError(400, "Invalid_data", CustomError.MODEL_TYPE_NOT_EXIST, "Model is not exist"));
         } else if (handlers[data[dataType]][data[actionStr]] === undefined || !(handlers[data.data_type][data.action_str] instanceof Function)) {
-            errorLog.insert(new CustomError(400, "Invalid_data", CustomError.ACTION_NOT_EXIST, "Action is not exist"));
+            apiResponse.addLog(new CustomError(400, "Invalid_data", CustomError.ACTION_NOT_EXIST, "Action is not exist"));
         }
-        if (errorLog.log_list.length === 1) return res.send(new ApiResponse("FAILED", "FORMAT", errorLog.log_list));
+        if (apiResponse.log_list.length === 1) return res.send(apiResponse.updateAndGet("FAILED", "FORMAT"));
 
         handlers[data[dataType]][data[actionStr]](req, res, function (err, apiResponse) {
             if (err) return next(err);
